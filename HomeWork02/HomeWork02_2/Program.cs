3a1d8e2c8410360b1c3ca3bfb3c8369e39adfadc
n0mercy13 <89132411+n0mercy13@users.noreply.github.com>
2022-08-05 00:49:44 +0300

    Homework for Lesson 2
    Tasks 1-6

---
+++ HomeWork02/HomeWork02_2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    internal class Program
+    {
+        /* Губин Андрей Игоревич
+         * 2. Написать метод подсчета количества цифр числа. */
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"There are {GetNumberOfDigits_String(number)} digit(s) in your number.");
+            Console.WriteLine($"There are {GetNumberOfDigits_Loop(number)} digit(s) in your number.");
+            Console.WriteLine($"There are {GetNumberOfDigits_Recursive(number)} digit(s) in your number.");
+            Console.ReadLine();
+        }
+
+        static int GetNumberOfDigits_String(int number)
+        {
+            int numberLength = number.ToString().Length;
+            
+            return (number >= 0) ? numberLength : numberLength - 1;
+        }
+
+        static int GetNumberOfDigits_Loop(int number)
+        {
+            int count = 0;
+            do
+            {
+                number /= 10;
+                count++;
+            } 
+            while (number != 0);
+
+            return count;
+        }
+
+        static int GetNumberOfDigits_Recursive(int number, int digits = 0)
+        {
+            if (number == 0 && digits == 0)
+                return 1;
+            if(number == 0)
+                return digits;
+            return GetNumberOfDigits_Recursive(number / 10, ++digits);
+        }
+
+    }
+}
---
+++ HomeWork02/Task06/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06
+{
+    internal class Program
+    {
+        /* 6. *Написать программу подсчета количества «хороших» чисел в диапазоне от 1 до 1 000 000
+            000. «Хорошим» называется число, которое делится на сумму своих цифр. Реализовать
+            подсчёт времени выполнения программы, используя структуру DateTime. */
+
+        static DateTime startTime;
+
+        static void Main(string[] args)
+        {
+            int range = 1_000_000_000;
+            startTime = DateTime.Now;
+
+            Console.WriteLine($"In range from 1 to {range} we have {CountGoodNumbers(range)} 'good' numbers");
+            CheckTimer(startTime);
+            Console.ReadLine();
+        }
+
+        static int CountGoodNumbers(int range)
+        {
+            int count = 0;
+            for (int i = 1; i <= range; i++)
+            {
+                if(IsNumberGood(i))
+                    count++;
+            }
+            return count;
+        }        
+
+        // Using Loop - fastest (33.33sec)
+
+        private static bool IsNumberGood(int number)
+        {
+            var originalNumber = number;
+            var sum = 0;
+            while (number != 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            return originalNumber % sum == 0;
+        }
+
+        // Using Recursive func - slow (125.95sec)
+
+        //private static bool IsNumberGood(int number)
+        //{
+        //    var sum = GetSumOfDigits_Recursive(number);
+
+        //    return number % sum == 0;
+        //}
+
+        // Using string & Linq - slowest (195.94sec)
+
+        //private static bool IsNumberGood(int number)
+        //{
+        //    var sum = number
+        //        .ToString()
+        //        .ToCharArray()
+        //        .Select(digit => (int)char.GetNumericValue(digit))
+        //        .Sum();
+        //    return number % sum == 0;
+        //}
+
+        private static int GetSumOfDigits_Recursive(int number)
+        {
+            if (number == 0)
+                return 0;
+
+            return number % 10 + GetSumOfDigits_Recursive(number / 10);
+        }
+
+        private static void CheckTimer(DateTime startTime)
+        {
+            Console.WriteLine($"Operation duration: {(DateTime.Now - startTime).TotalSeconds:F2} sec");
+        }
+    }
+}
