aca5588cf8379e9363ec0729d7256e5f8b50c23b
n0mercy13 <89132411+n0mercy13@users.noreply.github.com>
2022-08-11 12:41:22 +0300

    files updated

---
+++ HomeWork_02/HomeWork02_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    /* Губин Андрей Игоревич
+    1. Написать метод, возвращающий минимальное из трёх чисел. */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter 1st number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Please, enter 2nd number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Write("Please, enter 3rd number: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            int min = MinNumber(num1, num2, num3);
+            Console.WriteLine($"Minimum number is {min}");
+            Console.ReadLine();
+        }
+
+        static int MinNumber(int num1, int num2, int num3)
+        {
+            int min = num1 < num2 ? num1 : num2;
+            min = min < num3 ? min : num3;
+            return min;            
+        }
+    }
+}
---
+++ HomeWork_02/Task04/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    internal class Program
+    {
+        /* Губин Андрей Игоревич
+         * 4. Реализовать метод проверки логина и пароля. На вход метода подается логин и пароль. На
+            выходе истина, если прошел авторизацию, и ложь, если не прошел (Логин: root, Password:
+            GeekBrains). Используя метод проверки логина и пароля, написать программу: пользователь
+            вводит логин и пароль, программа пропускает его дальше или не пропускает. С помощью
+            цикла do while ограничить ввод пароля тремя попытками. */
+        static void Main(string[] args)
+        {            
+            int attemptsCounter = 3;
+
+            do
+            {
+                Console.WriteLine($"\nYou have {attemptsCounter} attempt(s) to log in.");
+                attemptsCounter--;
+
+                Console.Write("Login: ");
+                string loginInput = Console.ReadLine();
+                Console.Write("Password: ");
+                string passwordInput = Console.ReadLine();
+
+                if(IsAccessGranted(loginInput, passwordInput))
+                {                    
+                    Console.WriteLine($"Access granted.\nWelcome, {loginInput}.");
+                    Console.ReadLine();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine($"Wrong login or/and password.");                    
+                }
+            }
+            while(attemptsCounter > 0);
+
+            Console.WriteLine("Account was blocked. Have a nice day.");
+            Console.ReadLine();
+        }
+
+        static bool IsAccessGranted(string login, string password)
+        {
+            const string CORRECT_LOGIN = "root";
+            const string CORRECT_PASSWORD = "GeekBrains";
+
+            if(string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password)) return false;
+            return (login == CORRECT_LOGIN && password == CORRECT_PASSWORD) ? true : false;
+        }
+
+    }
+}
