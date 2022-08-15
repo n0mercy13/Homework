659e647a70aa9dd47d74a0366db87655166a709a
Gubin Andrei <hentai_ai@mail.ru>
2022-08-15 16:49:26 +0300

    files for homework04 uploaded

---
+++ Homework_04/Homework_04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Homework.Utility;
+
+namespace Homework_04
+{
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Action> tasks = new List<Action>()
+            {
+                Lesson04_Task01.RunTask01,
+                Lesson04_Task02.RunTask02,
+            };
+
+            new HomeworkAssist(4, tasks);
+        }
+    }
+}
