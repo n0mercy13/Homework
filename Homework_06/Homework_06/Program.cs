900bfcbe73a0ea8d3e4fe018df720ea848a6ae94
Gubin Andrei <hentai_ai@mail.ru>
2022-08-16 00:16:49 +0300

    - Homework06 Task01 uploaded; - Homework.Utility added;

---
+++ Homework_06/Homework_06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Homework.Utility;
+
+
+namespace Homework_06
+{
+    internal class Program
+    {
+        private static List<Action> _tasks = new List<Action>()
+        {
+            Lesson06_Task01.RunTask01,
+        };
+
+        static void Main(string[] args)
+        {
+            new HomeworkAssist(6, _tasks);
+        }
+    }
+}
