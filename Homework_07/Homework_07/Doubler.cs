3eec81de0bd73b7d68d3eb9fbd128d43b9efff73
Gubin Andrei <hentai_ai@mail.ru>
2022-08-24 15:54:09 +0300

    Homework07 : Task01 files uploaded

---
+++ Homework_07/Homework_07/Doubler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task01
+{
+    public class Doubler
+    {
+        private const int START_MAX = 25;
+        private const int GOAL_MAX = 200;
+        private Random _random;
+        private Stack<int> _savedTurns;
+        private int _currentNumber;
+        private int _goalNumber;
+        private int _currentTurn = 0;
+
+        public int CurrentNumber => _currentNumber;
+        public int GoalNumber => _goalNumber;
+        public int CurrentTurn => _currentTurn;
+
+        public Doubler()
+        {
+            _random = new Random();
+            _savedTurns = new Stack<int>();
+        }
+
+        public void StartNewGame()
+        {
+            _currentTurn = 0;
+            _savedTurns.Clear();
+            _currentNumber = _random.Next(START_MAX);
+            _goalNumber = _random.Next(START_MAX, GOAL_MAX);
+        }
+
+        public void AddOneToNumber()
+        {
+            SaveTurn();
+            _currentNumber++;
+            _currentTurn++;
+        } 
+        
+        public void MultiplyByTwoNumber()
+        {
+            SaveTurn();
+            _currentNumber *= 2;
+            _currentTurn++;
+        }
+
+        public bool IsGameOver()
+        {
+            return _currentNumber == _goalNumber;
+        }
+
+        public void UndoTurn()
+        {
+            if(_savedTurns.Count == 0)
+                return;
+            
+            _currentNumber = _savedTurns.Pop();
+            _currentTurn--;
+        }
+
+        private void SaveTurn()
+        {
+            _savedTurns.Push(_currentNumber);
+        }
+    }
+}
---
+++ Homework_07/Homework_07/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task01
+{
+    public partial class Form1 : Form
+    {
+        private Doubler _doubler;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            _doubler = new Doubler();
+            ToggleButtons();
+        }
+
+        private void StartNewGame()
+        {
+            _doubler.StartNewGame();
+            UpdateUI();
+            SetGoalMessage();
+            ToggleButtons();
+            ChangeStartButtonName();
+        }
+
+        private void SetGoalMessage()
+        {
+            labelNumberToReach.Text = $"Try to get number {_doubler.GoalNumber}";
+        }
+
+        private void SetGameOverMessage()
+        {
+            labelNumberToReach.Text = $"Superb you get it right!";
+        }
+
+        private void UpdateUI()
+        {
+            labelCurrentNumber.Text = _doubler.CurrentNumber.ToString();
+            labelCurrentTurn.Text = _doubler.CurrentTurn.ToString();
+        }
+
+        private void CheckIfGameOver()
+        {
+            if (_doubler.IsGameOver())
+            {
+                ToggleButtons();
+                SetGameOverMessage();
+                ChangeStartButtonName();
+            }
+        }
+
+        private void ChangeStartButtonName()
+        {
+            if (_doubler.IsGameOver())
+                buttonPlayRestart.Text = "Start";
+            else
+                buttonPlayRestart.Text = "Restart";
+        }
+
+        private void ToggleButtons()
+        {
+            buttonMultiplyByTwo.Enabled = !buttonMultiplyByTwo.Enabled;
+            buttonPlusOne.Enabled = !buttonPlusOne.Enabled;
+            buttonTurnBack.Enabled = !buttonTurnBack.Enabled;
+        }
+
+        private void buttonPlusOne_Click(object sender, EventArgs e)
+        {
+            _doubler.AddOneToNumber();
+            UpdateUI();
+            CheckIfGameOver();
+        }
+
+        private void buttonMultiplyByTwo_Click(object sender, EventArgs e)
+        {
+            _doubler.MultiplyByTwoNumber();
+            UpdateUI();
+            CheckIfGameOver();
+        }
+
+        private void buttonTurnBack_Click(object sender, EventArgs e)
+        {
+            _doubler.UndoTurn();
+            UpdateUI();            
+        }
+
+        private void buttonPlayRestart_Click(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+    }
+}
