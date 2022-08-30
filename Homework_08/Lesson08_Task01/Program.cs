44634655c5454d41353eff5320f62b64c4888682
Gubin Andrei <hentai_ai@mail.ru>
2022-08-30 12:18:57 +0300

    Homework 08: Task 01 and Task 02 uploaded

---
+++ Homework_08/Lesson08_Task01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+namespace Lesson08_Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Properties of 'DateTime' object: ");
+            PropertyInfo[] properties = GetPropertyInfo(new DateTime());
+            PrintOutPropeties(properties);
+        }
+
+        private static PropertyInfo[] GetPropertyInfo(object item)
+        {
+            return item.GetType().GetProperties();
+        }
+
+        private static void PrintOutPropeties(PropertyInfo[] properties)
+        {
+            foreach (var item in properties)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Homework_08/Lesson08_Task02/GameState.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson08_Task02
+{
+    public class GameState : BelieveOrNot
+    {
+        private const string SAVE = "QuickSave.json";
+
+        private int _currentQuestionNum = 0;
+        private int _totalQuestionNum;
+        private int _correctAnswers = 0;        
+        private float _successRate => 
+            (float)_correctAnswers * 100 / _totalQuestionNum;
+
+        public Action OnGameOver;
+        private Question _currentQuestion;        
+
+        public bool IsQuestionsBaseEmpty =>
+            _questionsBase.Count == 0;
+        public bool IsGameOver { get; private set; }
+
+        public string GetStatus() => 
+            $"Question: {_currentQuestionNum} / {_totalQuestionNum} (Success rate: {_successRate:F1}%)";
+
+        public override void StartNewGame()
+        {
+            base.StartNewGame();
+
+            _currentQuestionNum = 0;
+            _totalQuestionNum = _questionsGame.Count;
+            _correctAnswers = 0;
+            IsGameOver = false;
+
+
+            GetNextQuestion();
+        }
+
+        public void GetNextQuestion()
+        { 
+            if (_questionsGame.Count == 0)
+            {
+                OnGameOver?.Invoke();
+            }
+            else
+            {
+                _currentQuestionNum++;
+                _currentQuestion = _questionsGame.Pop();
+            }                       
+        }
+
+        public void CheckAnswer(bool answer)
+        {
+            if(answer == _currentQuestion.Answer)
+            {
+                _correctAnswers++;
+            }
+
+            SetGameOver();
+        }        
+
+        public void LoadGame(string saveName)
+        {
+            GameStateSave save = _handler.LoadSavedGame(saveName);
+            _correctAnswers = save.AnsweredQuestions;
+            _currentQuestionNum = save.CurrentTurn;
+            _questionsBase = save.QuestionsBase;
+            _questionsGame = save.QuestionsGame;
+            _currentQuestion = save.CurrentQuestion;
+        }
+
+        public void SaveGame(string saveName)
+        {
+            var save = new GameStateSave(_currentQuestionNum, _correctAnswers, 
+                _questionsGame, _questionsBase, _currentQuestion);
+            _handler.SaveGameStateAsync(save, saveName);
+        }
+
+        public void SaveGame() => SaveGame(SAVE);
+
+        public string GetQuestionText() => _currentQuestion.Text;
+
+        private void SetGameOver()
+        {
+            if(_questionsGame.Count == 0)
+            {
+                IsGameOver = true;
+            }
+        }
+    }
+}
